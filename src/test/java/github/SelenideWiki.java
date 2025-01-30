87efa4b167c069b56abb255ef7022d85a8cdbdb8
Ekaterina Chebotaeva <ek.chebotaeva@yandex.ru>
2025-01-30 22:21:04 +0500

    add test about content SelenideWiki

---
+++ src/test/java/github/SelenideWiki.java
@@ -0,0 +1,30 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideWiki {
+
+    @Test
+    void searchWikiSelenideInfoJUnitCode(){
+        open("https://github.com/selenide/selenide/");
+        $("#wiki-tab").click();
+        $("[data-filterable-for=wiki-pages-filter]").$(".wiki-more-pages-link").$(".Link--muted").click();
+        $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text(
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
